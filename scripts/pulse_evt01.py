8b6c25d7914bf9c6baaed7fb6e09cd9181b57d31
Fernando <fernandohds564@gmail.com>
2018-04-13 18:41:27 -0300

    improve pulse_evt01.

--- scripts/pulse_evt01.py
+++ scripts/pulse_evt01.py
@@ -2,6 +2,18 @@
 
 import time
 import epics
+import argparse as _argparse
+
+
+def configure_timing_modules(cycle=True):
+    print('Configuring Timing Modules to ' + ('cycle' if cycle else 'ramp'))
+    epics.caput('AS-Glob:TI-EVG:Evt01Mode-Sel', 'External')
+    epics.caput('AS-Glob:TI-EVG:DevEnbl-Sel', 1)
+    epics.caput('AS-Glob:TI-EVR-1DevEnbl-Sel', 1)
+    epics.caput('AS-Glob:TI-EVR-1OTP00Width-SP', 7000)
+    epics.caput('AS-Glob:TI-EVR-1OTP00State-Sel', 1)
+    epics.caput('AS-Glob:TI-EVR-1OTP00Evt-SP', 1)
+    epics.caput('AS-Glob:TI-EVR-1OTP00Pulses-SP', 1 if cycle else 4000)
 
 
 def run():
@@ -17,4 +29,15 @@ def run():
 
 
 if __name__ == '__main__':
+    parser = _argparse.ArgumentParser(description="Script for tests.")
+    parser.add_argument(
+        "configure", type=str, default='yes',
+        help="'yes' if you want to configure timing modules. Else 'no'")
+    parser.add_argument(
+        '-c', "--cycle", action='store_true', default=False,
+        help="If you want to perform cycling this must be present.")
+    args = parser.parse_args()
+
+    if args.configure.lower().startswith('y'):
+        configure_timing_modules(args.cycle)
     run()
