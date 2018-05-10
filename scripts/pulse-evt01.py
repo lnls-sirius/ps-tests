cc328557280c6ff0b46f389faff536bd3917c748
xresende <xresende@gmail.com>
2018-05-10 11:58:51 -0300

    Add mig mode in pulse-evt01.py

--- scripts/pulse-evt01.py
+++ scripts/pulse-evt01.py
@@ -44,6 +44,13 @@ def configure_timing_modules_ramp():
     write_pv(P+'AS-Glob:TI-EVG:Evt01Mode-Sel', 'Continuous')
     write_pv(P+'AS-Glob:TI-EVG:ContinuousEvt-Sel', 1)
 
+def configure_timing_modules_mig():
+    configure_timing_modules_init()
+    print('configuring timing modules (mig)')
+    write_pv(P+'AS-Glob:TI-EVR-1:OTP08Pulses-SP', WFMDATA_LENGTH)
+    write_pv(P+'AS-Glob:TI-EVG:Evt01Mode-Sel', 'External')
+    write_pv(P+'AS-Glob:TI-EVG:Evt01ExtTrig-Cmd', 1)
+
 def configure_timing_modules_stop():
     print('configuring timing modules (stop)')
     write_pv(P+'AS-Glob:TI-EVG:Evt01Mode-Sel', 'Disable')
@@ -74,9 +81,9 @@ if __name__ == '__main__':
     parser = _argparse.ArgumentParser(description="Script for tests.")
     parser.add_argument(
         "mode", type=str,
-        choices=('s', 'r', 'c', 'd', 'i'),
+        choices=('s', 'r', 'm', 'c', 'd', 'i'),
         help=
-            "s to stop ramp; r to ramp; c to cycle;" +
+            "s to stop ramp; r to ramp; m to mig; c to cycle;" +
             " d to read PVs; i init config timing")
     args = parser.parse_args()
 
@@ -93,3 +100,6 @@ if __name__ == '__main__':
     elif args.mode.lower().startswith('r'):
         configure_timing_modules_ramp()
         print('triggering timing in ramp mode (signal generator)...')
+    elif args.mode.lower().startswith('m'):
+        configure_timing_modules_mig()
+        print('triggering timing in mig mode (signal generator)...')
